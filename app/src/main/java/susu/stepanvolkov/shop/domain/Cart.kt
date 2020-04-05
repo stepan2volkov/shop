80275e3ad7ed38df36995b7b2f324e848de10f6b
Stepan Volkov <stepan2volkov@gmail.com>
2020-04-05 12:30:13 +0500

    DocString for Cart.getPrice modified a bit.

--- app/src/main/java/susu/stepanvolkov/shop/domain/Cart.kt
+++ app/src/main/java/susu/stepanvolkov/shop/domain/Cart.kt
@@ -2,7 +2,7 @@ package susu.stepanvolkov.shop.domain
 
 class Cart (var products: List<Product>) {
     /**
-     * @return total price of [products] in Cart with applied discount
+     * @return formatted total price of [products] in Cart with applied discount
      */
     fun getPrice(formatter: PriceFormatter): String {
         val totalPrice = products.sumByDouble { product -> product.calcDiscountPrice() }
