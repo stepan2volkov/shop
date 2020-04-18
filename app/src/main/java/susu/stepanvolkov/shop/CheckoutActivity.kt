e10ed10c061a7b22ac42b219aaf5ba7c6cc1f546
Stepan Volkov <stepan2volkov@gmail.com>
2020-04-18 13:54:02 +0500

    CheckoutActivity created.

---
+++ app/src/main/java/susu/stepanvolkov/shop/CheckoutActivity.kt
@@ -0,0 +1,12 @@
+package susu.stepanvolkov.shop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
