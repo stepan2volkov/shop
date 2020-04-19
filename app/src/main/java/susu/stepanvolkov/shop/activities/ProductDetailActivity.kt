89496e8b9e763629c96df7b5443cea13c6947c71
Stepan Volkov <stepan2volkov@gmail.com>
2020-04-19 16:08:46 +0500

    ProductDetailActivity created.

---
+++ app/src/main/java/susu/stepanvolkov/shop/activities/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package susu.stepanvolkov.shop.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
