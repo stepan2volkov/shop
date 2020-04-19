0d8dc5ab9127d4fb031d7e298bec56942b1612a8
Stepan Volkov <stepan2volkov@gmail.com>
2020-04-19 19:24:23 +0500

    Field ID added in Product Class.

--- app/src/main/java/susu/stepanvolkov/shop/Product.kt
+++ app/src/main/java/susu/stepanvolkov/shop/Product.kt
@@ -1,6 +1,7 @@
 package susu.stepanvolkov.shop
 
 class Product (
+    private val id: Int,
     private val name: String,
     private val price: Double,
     private val salePercent: Int = 0) {
@@ -11,19 +12,9 @@ class Product (
 
     fun calcDiscount(): Double = price * (salePercent / 100.0)
 
+    fun getId(): Int = id
+
     fun getName(): String = name
 
     fun getPrice(): Double = price
-
-    companion object {
-        /**
-         * @return list of products. Test implementation yet.
-         */
-        fun getProducts(): List<Product> {
-            val iphone = Product("iPhone", 73000.0, 7)
-            val samsung = Product("Samsung", 54000.0, 13)
-
-            return listOf(iphone, samsung)
-        }
-    }
 }
