c653d0b97f4820d4a059bb8c17d9f567000da175
Stepan Volkov <stepan2volkov@gmail.com>
2020-04-19 21:46:41 +0500

    CartActivity created.

---
+++ app/src/main/java/susu/stepanvolkov/shop/activities/CartActivity.kt
@@ -0,0 +1,13 @@
+package susu.stepanvolkov.shop.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import susu.stepanvolkov.shop.R
+
+class CartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
